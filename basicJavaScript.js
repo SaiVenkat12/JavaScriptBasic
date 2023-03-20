6bdbd8041bc60ee97f73046c944f8b9b289b2feb
SaiVenkat12 <gsaiv1001@gmail.com>
2023-03-20 22:27:56 +0530

    [Sai Venkat]- Added- Random Week Day

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -1,6 +1,6 @@
 //UC1- Generate Random Dice Value between 1 to 6
 let dice=Math.floor(Math.random() * 6)+1;
-console.log("Dice Value:"+dice);
+//console.log("Dice Value:"+dice);
 
 //UC2- Unit Conversion 
 // inches to feet 
@@ -8,18 +8,18 @@ function InchesToFeet(inches)
 {
     return inches/12;
 }
-console.log("Converting 42 inches into ft : " +InchesToFeet(42)); 
+//console.log("Converting 42 inches into ft : " +InchesToFeet(42)); 
 // Rectangular Plot of 60 x 40 feet in meters
 let ft1=60; let ft2=40;
 let meters1=ft1 /3.28; 
 let meters2=ft2 /3.28;
 let product=meters1 * meters2; 
-console.log("Conversion feet into meter: " + product); 
+//console.log("Conversion feet into meter: " + product); 
 // Calculate area of 25 such plots in acres
 let area=25;
 let result1=area / 43560;
-console.log("Result : " + result1);
-
+//console.log("Result : " + result1);
+//UC3 LeapYear or not
 function LeapYear(year){
 if(year % 400 == 0 || year % 4 == 0)
 {
@@ -34,4 +34,35 @@ else
    console.log(year+" is Not a Leap year");
 } 
 }
-console.log(LeapYear(2014));
+//console.log(LeapYear(2014));
+//UC4- Random Week Day
+let day = Math.floor(Math.random()*7);
+function WeekDay(day)
+{
+    
+    switch(day)
+    {
+    case 0:
+            console.log("Sunday");
+            break;
+    case 1:
+            console.log("Monday");
+            break;
+    case 2:
+            console.log("Tuesday");
+            break;
+    case 3:
+            console.log("Wednesday");
+            break;
+    case 4:
+            console.log("Thrusday");
+            break;
+    case 5:
+            console.log("Friday");
+            break;
+    case 6:
+            console.log(" Saturday");
+            break;
+    }
+}
+console.log(WeekDay(day))
